ac9324d6a0b01e535261b5f34e0f3f986063fee2
João Pedro Ribeiro da Silva Dias <78371415+JP2821@users.noreply.github.com>
2021-10-28 11:51:18 -0300

    Update salaDeChat.py

--- salaDeChat.py
+++ salaDeChat.py
@@ -7,17 +7,18 @@ s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM )
 s.bind(("127.0.0.1", 5000))
 print("\t\t\t====>  UDP CHAT APP  <=====")
 print("==============================================")
-nm = input("ENTER YOUR NAME : ")
-print("\nType 'quit' to exit.")
+nm = input("COLOQUE SEU NOME: ")
+print("\nDigite 'bye' para sair.")
 
-ip, port = input("Enter IP address and Port number: ").split()
+ip, port = input("Coloque seu Ip e a Porta: ").split()
 
 def send():
     while True:
         tempo = time.strftime('%H:%M:%S')
-        ms = input(">> ")
+        print(">>", end="")
+        ms = input(" ")
         if ms == "bye":
-            sm = "{} {}  : {}".format(tempo, nm, ms)
+            sm = "{} {} : {}".format(tempo, nm, ms)
             s.sendto(sm.encode(), (ip, int(port)))
             fechar_conexao()
         sm = "{} {}  : {}".format(tempo,nm,ms)
@@ -26,8 +27,8 @@ def send():
 def rec():
     while True:
         msg = s.recvfrom(1024)
-        print("\t\t\t\t >> " +  msg[0].decode()  )
-        print(">> ")
+        print(">> " +  msg[0].decode())
+
 x1 = threading.Thread( target = send )
 x2 = threading.Thread( target = rec )
 
