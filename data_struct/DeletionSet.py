7eb4db8bcce23ec2650f7cbcbff8f95448e8e0b8
Rodrigo Mota <raafm@cin.ufpe.br>
2021-11-20 14:31:05 -0300

    data struct creation O(N), delete O(1)

---
+++ data_struct/DeletionSet.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, data, next = None, prev = None): 
+        self.val = data
+        self.next = next
+        self.prev = prev
+    
+class DeletionSet: #circular linked list with array of address
+
+    def __init__(self,N=100):
+
+        self.nodeAddress = [None]*N
+        self.sentinela = Node(0) # node extra no inicio para poder exclui
+
+        last = Node(0,prev = self.sentinela)
+        self.sentinela.next = last
+        self.nodeAddress[0] = last
+
+        for i in range(1,N):
+            last.next = Node(i,prev = last)
+            
+            last = last.next
+            
+            self.nodeAddress[i] = last
+            
+
+        # link the end with the start
+        last.next = self.sentinela  
+        self.sentinela.prev = last
+
+
+    # nao apagar elemento ja apagado
+    def del_node(self,index):
+        node = self.nodeAddress[index]
+        
+        (node.prev).next = node.next
+        (node.next).prev = node.prev
+        
+
+    def parse(self) ->list:
+        cur = self.sentinela.next
+        not_deleted = list()
+        while cur != self.sentinela:
+            not_deleted.append(cur.val)
+            cur = cur.next
+        
+        return not_deleted
+
+
+#test code
+if __name__ == "__main__":
+
+    S = DeletionSet(5)
+    print(S.parse(),'\n')
+    while True:
+        entrada = input("0: sair\n1: deletar\ncomando:")
+        if entrada =='0': break
+        else:
+            try:
+                index = int(input("indice: "))
+            except:
+                continue
+            S.del_node(index)
+        print(S.parse(),'\n')
