6d0a47458251630188a5a9bae4645c5629e44b77
João Pedro Ribeiro da Silva Dias <78371415+JP2821@users.noreply.github.com>
2021-12-05 23:41:49 -0300

    Update cliente.py

--- protocoloRdt/cliente.py
+++ protocoloRdt/cliente.py
@@ -3,6 +3,15 @@ import sys
 import funcoes
 import time
 import random
+from threading import Thread
+
+# 0 - versao do codigo:
+while True:
+    try:
+        user_friendly = int(input("versao:\n0 para normal\n1 para user-friendly"))
+        break
+    except:
+        continue
 
 # 1 - Declarando as variaveis de cabeçalho
 host = 'localhost'
@@ -29,159 +38,76 @@ except socket.error:
     print("Datagrama não criado")
     sys.exit()
 
-portClient = 0 # Estou passando a porta como 0,pois o S.O. por sí só dá uma porta livre
+portClient = 0  # Estou passando a porta como 0,pois o S.O. por sí só dá uma porta livre
 client.bind(('', portClient))
-portaorigem = client.getsockname()[1] #usamos o metodo getsockname para acessar a porta forncida pelo S.O
+portaorigem = client.getsockname()[1]  # usamos o metodo getsockname para acessar a porta forncida pelo S.O
 
 file = open(sys.argv[1], 'r')
 array = file.read().splitlines()
 
-while len(array) > 1:
-    escolha = funcoes.menu(array)
-    while (escolha == 4):
-        toshuffle = array[0:len(array) - 1]
-        random.shuffle(toshuffle)
-        toshuffle.append('-1')
-        array = toshuffle
+# thread de escuta coloca dados do ack no buffer
+buffer = [None]
+
+# escuta so termina quando a main termina
+# enquanto isso fica ouvindo a todo tempo
+escuta = Thread(target=funcoes.esperar_ack, args=(client, tamanho_do_pacote, buffer))
+escuta.daemon = True
+escuta.start()
+
+# numero seq que deve ser enviado
+seq = 0
+
+for dados in array:
+    soma = funcoes.checksum(portaorigem, port, comprimento)
+    dados = int(dados)
+    if dados == -1: break
+
+    if user_friendly:
         escolha = funcoes.menu(array)
+        # Forçar o erro de bits mudando a soma de verificação
+        if escolha == 2:
+            soma += 42
+        # Forçando pacotes duplicados
+        elif escolha == 3:
+            seq = 1 - seq
+        # Encerrando cliente
+        elif escolha == 5:
+            sys.exit()
 
-
-    #Estado 0
-
-    dados = int(array.pop(0))
-    if dados == -1:
-        break
-
-    # Forçar o erro de bits mudando a soma de verificação
-    if escolha == 2:
-        soma = 0
-    else:
-        soma = funcoes.checksum(portaorigem, port, comprimento)
-
-    #Forçando pacotes duplicados
-    if escolha == 3:
-        seq = 1
-    else:
-        seq = 0
-
-    #Encerrando cliente
-    if escolha == 5:
-        sys.exit()
-
+    # cria mensagem e envia
     msg = funcoes.cria_pacote_cliente(portaorigem, port, comprimento, soma, seq, dados)
+    funcoes.enviar_msg(client, msg, host, port)
 
-    try: #Enviando o datagrama para o servidor
-        msg = msg.encode()
-        client.sendto(msg, (host, port))
-    except socket.error as msg:
-        print('Código do erro: ' + str(msg[0]) + '. Messagem: ' + msg[1])
-        sys.exit()
+    tcounter = 0  # time counter
+    while True:
 
-    # Estado 1
-    # Recebendo mensagem do servidor
-    data, address = client.recvfrom(tamanho_do_pacote)
+        time.sleep(0.001)  # espera 1 milisec
+        if tcounter == 100:  # time out: timer de 100 milisec
+            funcoes.enviar_msg(client, msg, host, port)
+            tcounter = 0  # reinicia timer
 
-    if not data:
-        break
+        else:
+            tcounter += 1  # esperar mais
 
-    portaorigemservidor , portadestinoservidor, comprimentoservidor , ackservidor  , seqservidor  , somaservidor  = funcoes.extrair_dados_cliente(data)
+        # mensagem nao chegou ainda
+        if buffer[0] is None: continue
 
-    vericar_soma = funcoes.checksum(portadestinoservidor, portadestinoservidor, comprimentoservidor)
+        data = buffer[0]
 
-    while (somaservidor != vericar_soma or (ackservidor == 1 and seqservidor == 1)):
-
-        soma = funcoes.checksum(portaorigem, port, comprimento)
-        seq = 0
-        msg = funcoes.cria_pacote_cliente(portaorigem, port, comprimento, soma, seq, dados)
-        msg = msg.encode()
-        client.sendto(msg, (host, port))
-
-        # Colocar thread para fazer isso.
-        # cronometrar o tempo ate chegar, 
-        # quando a mensagem chegar
-        # a thread que instanciamos 
-        # faz a atual parar de esperar
-
-        # Recebendo a mensagem do servidor
-        data,address = client.recvfrom(tamanho_do_pacote)
-      
-        portaorigemservidor , portadestinoservidor, comprimentoservidor , ackservidor  , seqservidor  , somaservidor = funcoes.extrair_dados_cliente(data)
-
-        vericar_soma = funcoes.checksum(portaorigemservidor, portadestinoservidor, comprimentoservidor)
-
-        if not data: #fim da comunicacao
-            break
-
-    #Estado 2
-
-    escolha = funcoes.menu(array)
-    while (escolha == 4):
-        toshuffle = array[0:len(array) - 1]
-        random.shuffle(toshuffle)
-        toshuffle.append('-1')
-        array = toshuffle
-        escolha = funcoes.menu(array)
-
-    dado = int(array.pop(0))
-    if dado == -1:
-        break
-
-    # Forçar o erro de bits mudando a sma de verificação
-    if escolha == 2:
-        soma = 0
-    else:
-        soma = funcoes.checksum(portaorigem, port, comprimento)
-
-    #Forçando pacotes duplicados
-    if escolha == 3:
-        seq = 1
-    else:
-        seq = 0
-
-    #Encerrando cliente
-    if escolha == 5:
-        sys.exit()
-
-    msg = funcoes.cria_pacote_cliente(portaorigem, port, comprimento, soma, seq, dados)
-
-    try:
-        #Enviando datagrama ao servidor
-        msg = msg.encode()
-        client.sendto(msg, (host, port))
-    except socket.error as msg:
-        print('Código do erro: ' + str(msg[0]) + '. Messagem: ' + msg[1])
-        sys.exit()
-
-    #Estado 3
-    # Recebendo mensagem do servidor
-
-    data, address = client.recvfrom(tamanho_do_pacote)
-
-    if not data:
-        break
-
-    portaorigemservidor , portadestinoservidor, comprimentoservidor , ackservidor  , seqservidor  , somaservidor = funcoes.extrair_dados_cliente(data)
-
-    vericar_soma = funcoes.checksum(portadestinoservidor, portadestinoservidor, comprimentoservidor)
-
-    while (somaservidor != vericar_soma or (ackservidor == 1 and seqservidor == 0)):
-
-        soma = funcoes.checksum(portaorigem, port, comprimento)
-        seq = 1
-        msg = funcoes.cria_pacote_cliente(portaorigem, port, comprimento, soma, seq, dado)
-        msg = msg.encode()
-        client.sendto(msg, (host, port))
-
-        #Recebendo a mensagem do servidor
-        data, address = client.recvfrom(tamanho_do_pacote)
-        
-        if not data:
-            break
-
-        portaorigemservidor , portadestinoservidor, comprimentoservidor , ackservidor  , seqservidor  , somaservidor = funcoes.extrair_dados_cliente(data)
+        portaorigemservidor, portadestinoservidor, comprimentoservidor, ackservidor, seqservidor, somaservidor = funcoes.extrair_dados_cliente(
+            data)
 
         vericar_soma = funcoes.checksum(portadestinoservidor, portadestinoservidor, comprimentoservidor)
 
+        # pacote com erros
+        if somaservidor != vericar_soma or (ackservidor == 1 - seq):
+            tcounter = 0  # reinicia o timer
+            funcoes.enviar_msg(client, msg, host, port)
 
+        # pacote chegou sem erros
+        else:
+            buffer[0] = None  # buffer indicando que prox pacote ainda nao chegou
+            seq = 1 - seq  # seq modificado para proximo pacote
+            break
 
 client.close()
